67f6616032cc229b12a1b454e8076d8b2c49196e
baldore <acornssn@gmail.com>
2016-07-26 18:21:17 -0500

    WIP - Adds babel and scss loader (no webpack-dev-server)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 'use strict';
 
+require('../styles/main.scss');
+
 const users = require('./data');
 const renderUsers = require('./renderUsers');
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,7 @@
+'use strict';
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 module.exports = {
   entry: './src/main.js',
   output: {
@@ -6,13 +10,24 @@ module.exports = {
   },
   module: {
     loaders: [
-
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap', 'sass'),
+        exclude: /node_modules/
+      }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true })
+  ]
 };
 
 // Loaders
-// - Babel
 // - Styles
 // - Handlebars
 
